e17830c059189a30fd2710052d0e3af5de6252e2
Marcos Gabriel <marcosgabriel79355@gmail.com>
2023-10-02 22:19:16 -0300

    Initial commit

---
+++ src/test/java/br/com/apicosulta/consultaapi/ApiDeConsultaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.apicosulta.consultaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDeConsultaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
