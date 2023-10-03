e7c472e03401eca9d254c76e43aae35fd7509bc2
Marcos Gabriel <marcosgabriel79355@gmail.com>
2023-10-03 20:40:28 -0300

    Começando a criar a class de config

---
+++ src/main/java/br/com/apicosulta/consultaapi/config/Config.java
@@ -0,0 +1,7 @@
+package br.com.apicosulta.consultaapi.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Config {
+}
--- src/main/java/br/com/apicosulta/consultaapi/service/CepService.java
+++ src/main/java/br/com/apicosulta/consultaapi/service/CepService.java
@@ -1,4 +1,19 @@
 package br.com.apicosulta.consultaapi.service;
 
+import br.com.apicosulta.consultaapi.model.CepModel;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
 public class CepService {
+
+    private final RestTemplate restTemplate;
+
+    public CepService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    public CepModel consultar(String cep) {
+
+    }
 }
