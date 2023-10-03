1bc019cbc36725e8d7b7ef6c21e0da0adfaeb017
Marcos Gabriel <marcosgabriel79355@gmail.com>
2023-10-03 20:56:26 -0300

    Finalizando o controller do cep

--- src/main/java/br/com/apicosulta/consultaapi/controller/CepController.java
+++ src/main/java/br/com/apicosulta/consultaapi/controller/CepController.java
@@ -1,4 +1,23 @@
 package br.com.apicosulta.consultaapi.controller;
 
-public class CepController {
+import br.com.apicosulta.consultaapi.model.CepModel;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class Config {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
 }
