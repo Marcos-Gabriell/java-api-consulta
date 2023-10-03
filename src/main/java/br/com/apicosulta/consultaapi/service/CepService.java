9231058384c03a4924aa95e3a23fb377ae6ed920
Marcos Gabriel <marcosgabriel79355@gmail.com>
2023-10-03 20:49:34 -0300

    Finalziando class de  serviceCep

--- src/main/java/br/com/apicosulta/consultaapi/service/CepService.java
+++ src/main/java/br/com/apicosulta/consultaapi/service/CepService.java
@@ -7,13 +7,17 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class CepService {
 
-    private final RestTemplate restTemplate;
 
-    public CepService(RestTemplate restTemplate) {
-        this.restTemplate = restTemplate;
+
+        private final RestTemplate restTemplate;
+
+        public CepService(RestTemplate restTemplate) {
+            this.restTemplate = restTemplate;
+        }
+
+        public CepModel consultar(String cep) {
+            CepModel cepModel = restTemplate.getForEntity("https://viacep.com.br/ws/" + cep + "/json/", CepModel.class).getBody();
+            cepModel.setCep(cepModel.getCep().replace("-", ""));
+            return cepModel;
+        }
     }
-
-    public CepModel consultar(String cep) {
-
-    }
-}
