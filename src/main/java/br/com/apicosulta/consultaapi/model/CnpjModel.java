03daf0c97ab7608eb0872bd5e6d02c6e34a4ac20
Marcos Gabriel <marcosgabriel79355@gmail.com>
2023-10-04 14:41:52 -0300

    Revert "Finalziando a class do model do CNPJ"
    This reverts commit 64583dcda580c9199484835e007faa8a02354cab.

--- src/main/java/br/com/apicosulta/consultaapi/model/CnpjModel.java
+++ src/main/java/br/com/apicosulta/consultaapi/model/CnpjModel.java
@@ -1,20 +1,4 @@
 package br.com.apicosulta.consultaapi.model;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Data;
-
-import java.io.Serializable;
-import java.util.List;
-
-@Data
-public class CnpjModel implements Serializable {
-
-    @JsonProperty("razao_social")
-    private String razaoSocial;
-
-    @JsonProperty("nome_fantasia")
-    private String nomeFantasia;
-
-    @JsonProperty("cnaes_secundarios")
-    private List<CnaeModel> cnaesSecundarios;
+public class CnpjModel {
 }
