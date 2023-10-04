4c344e41def30e4b0e250a2737245a25e011ff98
Marcos Gabriel <marcosgabriel79355@gmail.com>
2023-10-04 16:10:00 -0300

    Coreção

--- src/main/java/br/com/apicosulta/consultaapi/controller/CnpjController.java
+++ src/main/java/br/com/apicosulta/consultaapi/controller/CnpjController.java
@@ -1,1 +1,26 @@
-package br.com.apicosulta.consultapi.controller;
+package br.com.apicosulta.consultaapi.controller;
+
+import br.com.apicosulta.consultaapi.model.CnpjModel;
+import br.com.apicosulta.consultaapi.service.CnpjService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cnpj")
+public class CnpjController {
+
+    private final CnpjService cnpjService;
+
+    public CnpjController(CnpjService cnpjService) {
+        this.cnpjService = cnpjService;
+    }
+
+    @GetMapping("/consulta/{cnpj}")
+    public ResponseEntity<CnpjModel> consulta(@PathVariable String cnpj) {
+        return new ResponseEntity<>(cnpjService.consultar(cnpj), HttpStatus.OK);
+    }
+}
