ae9251cffa9a82506bb59cf15a8446f4b4228a87
Marcos Gabriel <marcosgabriel79355@gmail.com>
2023-10-04 14:27:17 -0300

    Varios erros agr

--- src/main/java/br/com/apicosulta/consultaapi/model/CepModel.java
+++ src/main/java/br/com/apicosulta/consultaapi/model/CepModel.java
@@ -1,16 +1,4 @@
 package br.com.apicosulta.consultaapi.model;
 
-import lombok.Data;
-
-import java.io.Serializable;
-
-@Data
-public class CNPJModelCepModel implements Serializable {
-
-    private String cep;
-    private String logradouro;
-    private String complemento;
-    private String bairro;
-    private String localidade;
-    private String uf;
+public class CepModel {
 }
